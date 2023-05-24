08aad6b4d9bdc4f9a4ff083d00ff7588cc958b72
Statick <dsaavedra88@gmail.com>
2023-05-24 10:34:43 -0500

    if

---
+++ Condicionales/if.py
@@ -0,0 +1,32 @@
+if 2>8:
+    print("2 es mayor que 8")
+elif 10 > 20:
+    print("10 es menor a 20")
+elif 100 <= 80:
+    print("100 es mayor o igual que 80")
+elif 80 >= 100:
+    print("80 es menor o igual que 100")
+elif 100 != 100:
+    print("100 es diferente de 20")
+elif 100 == 1000:
+    print("100 es igual a 100")
+else:
+    print("No se cumplio ninguna condicion anterior")
+
+# Operadores Ternarios
+
+if 2 < 10: print("2 es menor a 5")
+
+print("Se imprime si la condición es verdadera") if 10 > 2 else print("Se imprime si la condición es falsa")
+
+# And
+
+if 2<9 and 10<20:
+    print("2 es mayor que 9 y 10 es menor que 20")
+
+if 100 < 1000 or 20 > 900:
+    print("100 es menor que 100 o 20 es mayor que 900")
+
+if not 10 < 9:
+    print("No es verdad que 10 es menor que 9   ")
+
