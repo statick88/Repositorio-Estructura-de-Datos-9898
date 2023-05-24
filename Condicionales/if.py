cf9bf3ccff2eee1c85751e149f43ade4df53dee7
[DavidCr15] <[cpincha9@gmail.com]>
2023-05-24 10:00:41 -0500

    enviado

---
+++ Condicionales/if.py
@@ -0,0 +1,14 @@
+if 2>8:
+    print("2 es mayor que 8")
+elif 10<20:
+    print("10 es menor que 20")
+elif 100>=80:
+    print("100 es mayor o igual que 80")
+elif 80<=100:
+    print("80 es menor o igual que 100")
+elif 100!=20:
+    print("100 es diferente de 20")
+elif 100==20:
+    print("100 es igual a 100")
+else:
+    print("no se cumplio ninguna condicion anterior")
