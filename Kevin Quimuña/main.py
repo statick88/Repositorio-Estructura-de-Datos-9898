317e3770e835af32c215cf5946ee1c89a539f5cc
KevinQuimuna <121051282+KevinQuimuna@users.noreply.github.com>
2023-06-14 10:22:31 -0500

    Peliculas por mascotas

--- Kevin Quimuña/main.py
+++ Kevin Quimuña/main.py
@@ -1,4 +1,4 @@
-class Pelicula:
+class Mascotas:
     def __init__(self, nombre, anio):
         self.nombre = nombre
         self.anio = anio
@@ -7,178 +7,177 @@ class Pelicula:
         return f"{self.nombre} ({self.anio})"
 
 
-def mostrar_peliculas(peliculas):
-    if peliculas:
-        print("Películas ingresadas:")
-        for pelicula in peliculas:
-            print(pelicula)
+def mostrar_mascotas(mascotas):
+    if mascotas:
+        print("Mascotas ingresadas:")
+        for mascota in mascotas:
+            print(mascota)
     else:
-        print("No se han ingresado películas.")
+        print("No se han ingresado mascotas.")
 
 
-def eliminar_pelicula(peliculas):
-    if peliculas:
-        nombre = input("Ingrese el nombre de la película a eliminar: ")
-        peliculas_filtradas = [pelicula for pelicula in peliculas if pelicula.nombre != nombre]
-        if len(peliculas_filtradas) < len(peliculas):
-            peliculas[:] = peliculas_filtradas
-            print("Película eliminada exitosamente.")
+def eliminar_mascotas(mascotas):
+    if mascotas:
+        nombre = input("Ingrese el nombre de la mascota a eliminar: ")
+        mascotas_filtradas = [mascota for mascota in mascotas if mascota.nombre != nombre]
+        if len(mascotas_filtradas) < len(mascotas):
+            mascotas[:] = mascotas_filtradas
+            print("Mascota eliminada exitosamente.")
         else:
-            print("No se encontró una película con ese nombre.")
+            print("No se encontró una mascota con ese nombre.")
     else:
-        print("No se han ingresado películas.")
+        print("No se han ingresado mascotas.")
 
 
-
-def modificar_pelicula(peliculas):
-    if peliculas:
-        nombre = input("Ingrese el nombre de la película a modificar: ")
-        for pelicula in peliculas:
-            if pelicula.nombre == nombre:
-                nuevo_nombre = input("Ingrese el nuevo nombre de la película: ")
-                nuevo_anio = int(input("Ingrese el nuevo año de la película: "))
-                pelicula.nombre = nuevo_nombre
-                pelicula.anio = nuevo_anio
-                print("Película modificada exitosamente.")
+def modificar_mascota(mascotas):
+    if mascotas:
+        nombre = input("Ingrese el nombre de la mascota a modificar: ")
+        for mascota in mascotas:
+            if mascota.nombre == nombre:
+                nuevo_nombre = input("Ingrese el nuevo nombre de la mascota: ")
+                nuevo_anio = int(input("Ingrese el nuevo año de la mascota: "))
+                mascota.nombre = nuevo_nombre
+                mascota.anio = nuevo_anio
+                print("Mascota modificada exitosamente.")
                 return
-        print("No se encontró una película con ese nombre.")
+        print("No se encontró una mascota con ese nombre.")
     else:
-        print("No se han ingresado películas.")
+        print("No se han ingresado mascotas.")
 
 
-def ordenar_peliculas(peliculas, criterio):
-    if peliculas:
+def ordenar_mascotas(mascotas, criterio):
+    if mascotas:
         if criterio == "nombre":
             algoritmo = input("Ingrese el algoritmo de ordenamiento a utilizar (burbuja, shell, quick): ")
             if algoritmo == "burbuja":
-                peliculas.sort(key=lambda pelicula: pelicula.nombre)
+                mascotas.sort(key=lambda mascota: mascota.nombre)
             elif algoritmo == "shell":
                 # Implementar el algoritmo de ordenamiento ShellSort por nombre
-                def shell_sort_nombre(peliculas):
-                    n = len(peliculas)
+                def shell_sort_nombre(mascotas):
+                    n = len(mascotas)
                     gap = n // 2
 
                     while gap > 0:
                         for i in range(gap, n):
-                            temp = peliculas[i]
+                            temp = mascotas[i]
                             j = i
 
-                            while j >= gap and peliculas[j - gap].nombre > temp.nombre:
-                                peliculas[j] = peliculas[j - gap]
+                            while j >= gap and mascotas[j - gap].nombre > temp.nombre:
+                                mascotas[j] = mascotas[j - gap]
                                 j -= gap
 
-                            peliculas[j] = temp
+                            mascotas[j] = temp
 
                         gap //= 2
 
-                shell_sort_nombre(peliculas)
+                shell_sort_nombre(mascotas)
             elif algoritmo == "quick":
                 # Implementar el algoritmo de ordenamiento Quicksort por nombre
-                def partition_nombre(peliculas, low, high):
-                    pivot = peliculas[high].nombre
+                def partition_nombre(mascotas, low, high):
+                    pivot = mascotas[high].nombre
                     i = low - 1
 
                     for j in range(low, high):
-                        if peliculas[j].nombre < pivot:
+                        if mascotas[j].nombre < pivot:
                             i += 1
-                            peliculas[i], peliculas[j] = peliculas[j], peliculas[i]
+                            mascotas[i], mascotas[j] = mascotas[j], mascotas[i]
 
-                    peliculas[i + 1], peliculas[high] = peliculas[high], peliculas[i + 1]
+                    mascotas[i + 1], mascotas[high] = mascotas[high], mascotas[i + 1]
                     return i + 1
 
-                def quick_sort_nombre(peliculas, low, high):
+                def quick_sort_nombre(mascotas, low, high):
                     if low < high:
-                        pivot_index = partition_nombre(peliculas, low, high)
-                        quick_sort_nombre(peliculas, low, pivot_index - 1)
-                        quick_sort_nombre(peliculas, pivot_index + 1, high)
+                        pivot_index = partition_nombre(mascotas, low, high)
+                        quick_sort_nombre(mascotas, low, pivot_index - 1)
+                        quick_sort_nombre(mascotas, pivot_index + 1, high)
 
-                quick_sort_nombre(peliculas, 0, len(peliculas) - 1)
+                quick_sort_nombre(mascotas, 0, len(mascotas) - 1)
             else:
                 print("Algoritmo de ordenamiento inválido.")
         elif criterio == "anio":
             algoritmo = input("Ingrese el algoritmo de ordenamiento a utilizar (burbuja, shell, quick): ")
             if algoritmo == "burbuja":
-                peliculas.sort(key=lambda pelicula: pelicula.anio)
+                mascotas.sort(key=lambda mascota: mascota.anio)
             elif algoritmo == "shell":
                 # Implementar el algoritmo de ordenamiento ShellSort por año
-                def shell_sort_anio(peliculas):
-                    n = len(peliculas)
+                def shell_sort_anio(mascotas):
+                    n = len(mascotas)
                     gap = n // 2
 
                     while gap > 0:
                         for i in range(gap, n):
-                            temp = peliculas[i]
+                            temp = mascotas[i]
                             j = i
 
-                            while j >= gap and peliculas[j - gap].anio > temp.anio:
-                                peliculas[j] = peliculas[j - gap]
+                            while j >= gap and mascotas[j - gap].anio > temp.anio:
+                                mascotas[j] = mascotas[j - gap]
                                 j -= gap
 
-                            peliculas[j] = temp
+                            mascotas[j] = temp
 
                         gap //= 2
 
-                shell_sort_anio(peliculas)
+                shell_sort_anio(mascotas)
             elif algoritmo == "quick":
                 # Implementar el algoritmo de ordenamiento Quicksort por año
-                def partition_anio(peliculas, low, high):
-                    pivot = peliculas[high].anio
+                def partition_anio(mascotas, low, high):
+                    pivot = mascotas[high].anio
                     i = low - 1
 
                     for j in range(low, high):
-                        if peliculas[j].anio < pivot:
+                        if mascotas[j].anio < pivot:
                             i += 1
-                            peliculas[i], peliculas[j] = peliculas[j], peliculas[i]
+                            mascotas[i], mascotas[j] = mascotas[j], mascotas[i]
 
-                    peliculas[i + 1], peliculas[high] = peliculas[high], peliculas[i + 1]
+                    mascotas[i + 1], mascotas[high] = mascotas[high], mascotas[i + 1]
                     return i + 1
 
-                def quick_sort_anio(peliculas, low, high):
+                def quick_sort_anio(mascotas, low, high):
                     if low < high:
-                        pivot_index = partition_anio(peliculas, low, high)
-                        quick_sort_anio(peliculas, low, pivot_index - 1)
-                        quick_sort_anio(peliculas, pivot_index + 1, high)
+                        pivot_index = partition_anio(mascotas, low, high)
+                        quick_sort_anio(mascotas, low, pivot_index - 1)
+                        quick_sort_anio(mascotas, pivot_index + 1, high)
 
-                quick_sort_anio(peliculas, 0, len(peliculas) - 1)
+                quick_sort_anio(mascotas, 0, len(mascotas) - 1)
             else:
                 print("Algoritmo de ordenamiento inválido.")
         else:
             print("Criterio de ordenamiento inválido.")
     else:
-        print("No se han ingresado películas.")
+        print("No se han ingresado mascotas.")
 
 
 def menu():
-    peliculas = []
+    mascotas = []
 
     while True:
         print("------ MENÚ ------")
-        print("1. Ingresar película")
-        print("2. Mostrar películas")
-        print("3. Eliminar película")
-        print("4. Modificar película")
-        print("5. Ordenar películas por nombre")
-        print("6. Ordenar películas por año")
+        print("1. Ingresar mascota")
+        print("2. Mostrar mascotas")
+        print("3. Eliminar mascota")
+        print("4. Modificar mascota")
+        print("5. Ordenar mascotas por nombre")
+        print("6. Ordenar mascotas por año")
         print("0. Salir")
 
         opcion = input("Ingrese una opción: ")
 
         if opcion == "1":
-            nombre = input("Ingrese el nombre de la película: ")
-            anio = int(input("Ingrese el año de la película: "))
-            pelicula = Pelicula(nombre, anio)
-            peliculas.append(pelicula)
-            print("Película ingresada exitosamente.")
+            nombre = input("Ingrese el nombre de la mascota: ")
+            anio = int(input("Ingrese el año de la mascota: "))
+            mascota = Mascotas(nombre, anio)
+            mascotas.append(mascota)
+            print("Mascota ingresada exitosamente.")
         elif opcion == "2":
-            mostrar_peliculas(peliculas)
+            mostrar_mascotas(mascotas)
         elif opcion == "3":
-            eliminar_pelicula(peliculas)
+            eliminar_mascotas(mascotas)
         elif opcion == "4":
-            modificar_pelicula(peliculas)
+            modificar_mascota(mascotas)
         elif opcion == "5":
-            ordenar_peliculas(peliculas, "nombre")
+            ordenar_mascotas(mascotas, "nombre")
         elif opcion == "6":
-            ordenar_peliculas(peliculas, "anio")
+            ordenar_mascotas(mascotas, "anio")
         elif opcion == "0":
             print("¡Hasta luego!")
             break
